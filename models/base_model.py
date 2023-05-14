f5c6b463340e755089ed732c6d154b188fa3d2c4
Haile-08 <haile4cmd@gmail.com>
2023-05-14 09:51:34 +0000

    Change datetime to utc date

--- models/base_model.py
+++ models/base_model.py
@@ -22,12 +22,12 @@ class BaseModel:
                     pass
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """update the public instance attribute updated_at"""
-        self.updated_at = datetime.now();
+        self.updated_at = datetime.utcnow();
  
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
